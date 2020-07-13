689b741ed065cfb9b9e6f460e1962f6497c455c9
Adekogbe Faith <28295747+Phainix@users.noreply.github.com>
2020-07-13 21:56:00 +0100

    Week 5

---
+++ week6/validation/self.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<pre>
+<?php var_dump(json_encode(array(htmlspecialchars($_SERVER["PHP_SELF"])))); die;?>
+<!-- <script>window.location.href='https://youtube.com';</script> -->
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+</form>
+</body>
+</html>
